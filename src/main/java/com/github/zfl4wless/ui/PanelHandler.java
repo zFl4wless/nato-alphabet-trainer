3403739bb8a321249d55e3c898e8a18c0e414730
zFl4wless <noah.bauditz@live.de>
2022-07-13 22:05:00 +0200

    refactor: changes every "page" to "panel"

--- src/main/java/com/github/zfl4wless/ui/PanelHandler.java
+++ src/main/java/com/github/zfl4wless/ui/PanelHandler.java
@@ -3,15 +3,15 @@ package com.github.zfl4wless.ui;
 import javax.swing.*;
 import java.awt.*;
 
-public class PageHandler {
+public class PanelHandler {
     private final JFrame mainFrame;
-    private JPanel currentPage;
+    private JPanel currentPanel;
 
-    public PageHandler() {
+    public PanelHandler() {
         this.mainFrame = new MainFrame();
     }
 
-    public JPanel createPage() {
+    public JPanel createPanel() {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(60, 120, 60, 120));
         panel.setLayout(new GridLayout(0, 1));
@@ -19,20 +19,20 @@ public class PageHandler {
         return panel;
     }
 
-    public void showPage(JPanel page) {
-        if (this.currentPage != null) {
-            this.mainFrame.remove(this.currentPage);
+    public void showPanel(JPanel panel) {
+        if (this.currentPanel != null) {
+            this.mainFrame.remove(this.currentPanel);
         }
 
-        this.mainFrame.add(page, BorderLayout.CENTER);
-        this.currentPage = page;
+        this.mainFrame.add(panel, BorderLayout.CENTER);
+        this.currentPanel = panel;
     }
 
     public JFrame getMainFrame() {
         return this.mainFrame;
     }
 
-    public JPanel getCurrentPage() {
-        return this.currentPage;
+    public JPanel getCurrentPanel() {
+        return this.currentPanel;
     }
 }
