2ead05b3648155eee1f9018a758cd699094436b0
zFl4wless <noah.bauditz@live.de>
2022-07-13 22:19:17 +0200

    docs: adds missing doumentation

--- src/main/java/com/github/zfl4wless/ui/PanelHandler.java
+++ src/main/java/com/github/zfl4wless/ui/PanelHandler.java
@@ -11,6 +11,11 @@ public class PanelHandler {
         this.mainFrame = new MainFrame();
     }
 
+    /**
+     * Creates a new panel.
+     *
+     * @return The created panel.
+     */
     public JPanel createPanel() {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(60, 120, 60, 120));
@@ -19,6 +24,11 @@ public class PanelHandler {
         return panel;
     }
 
+    /**
+     * Renders a new panel to the main frame.
+     *
+     * @param panel The panel.
+     */
     public void showPanel(JPanel panel) {
         if (this.currentPanel != null) {
             this.mainFrame.remove(this.currentPanel);
@@ -28,10 +38,20 @@ public class PanelHandler {
         this.currentPanel = panel;
     }
 
+    /**
+     * Gets the main frame.
+     *
+     * @return The main frame.
+     */
     public JFrame getMainFrame() {
         return this.mainFrame;
     }
 
+    /**
+     * Gets the current panel.
+     *
+     * @return The current panel.
+     */
     public JPanel getCurrentPanel() {
         return this.currentPanel;
     }
