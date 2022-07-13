4709add4f08d54c57751aba0d7cc40ac1b942975
zBinFinn <84500718+zBinFinn@users.noreply.github.com>
2022-07-13 20:45:17 +0200

    Reworks Random Word Generator (#13)
    * build: removes randomwordgenerator dependency
    
    * feat: adds the word list used in the randomwordgen
    
    * feat: adds the randomwordgen

--- src/main/java/com/github/zfl4wless/utils/RandomWordGen.java
+++ src/main/java/com/github/zfl4wless/utils/RandomWordGen.java
@@ -1,16 +1,26 @@
 package com.github.zfl4wless.utils;
 
-import com.github.dhiraj072.randomwordgenerator.RandomWordGenerator;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Objects;
+import java.util.Random;
 
 public class RandomWordGen {
 
-    public static String getRandomString() {
-        String word;
+    private static JSONObject wordsObject;
+    private static JSONArray wordsArray;
+    private static Random random = new Random();
 
-        do {
-            word = RandomWordGenerator.getRandomWord();
-        } while (word.length() > 10);
+    public RandomWordGen() {
+        wordsObject = new JSONObject(
+                Objects.requireNonNull(FIleUtils.readResource("words.json"))
+        );
+        wordsArray = wordsObject.getJSONArray("nouns");
 
-        return word;
+    }
+
+    public String getRandomString() {
+        return wordsArray.getString(random.nextInt(wordsArray.length() - 1));
     }
 }
